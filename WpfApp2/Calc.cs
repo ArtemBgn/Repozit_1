0f8c5f7e98a27fc9cf97d4a26e00edda0fcd0e5d
ArtemBgn <bgn.rtm@gmail.com>
2023-09-02 23:33:47 +0300

    Update 02.09.2023 in 23:33

---
+++ WpfApp2/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public static class Calc
+    {
+        public static decimal sum(double x, double y)
+        {
+            return (decimal)x + (decimal)y;
+        }
+        public static decimal sub(double x, double y)
+        {
+            return (decimal)x - (decimal)y;
+        }
+        public static decimal mult(double x, double y)
+        {
+            return (decimal)x * (decimal)y;
+        }
+        public static decimal div(double x, double y)
+        {
+
+            return (decimal)x / (decimal)y;
+        }
+        public static decimal remOfdiv(double x, double y)
+        {
+            return (decimal)x % (decimal)y;
+        }
+    }
+}
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -22,7 +22,63 @@ namespace WpfApp2
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            decimal a = -1;
+
+            double x;
+            double y;
+            bool b1 = double.TryParse(textbox1.Text, out x);
+            bool c1 = double.TryParse(textbox2.Text, out y);
+            //MessageBox.Show($"текстбоксс 1 = {b1}\nтекстбокс 2 = {c1}");
+
+            if (b1 && c1)
+            {
+                if ((double.Parse(textbox1.Text) > double.MaxValue) || (double.Parse(textbox1.Text) < double.MinValue))
+                {
+                    MessageBox.Show("Ви ввели занадто велеке значення!\nБудь-ласка, спробуйте інше значення..");
+                    return;
+                }
+
+                if ((double.Parse(textbox2.Text) > double.MaxValue) || (double.Parse(textbox2.Text) < double.MinValue))
+                {
+                    MessageBox.Show("Ви ввели занадто мале значення!\nБудь-ласка, спробуйте інше значення..");
+                    return;
+                }
+
+                switch (combobox1.SelectedIndex)
+                {
+                    case (0):
+                        a = Calc.sum(x, y);
+                        break;
+                    case (1):
+                        a = Calc.sub(x, y);
+                        break;
+                    case (2):
+                        a = Calc.mult(x, y);
+                        break;
+                    case (3):
+                        if (y == 0)
+                        {
+                            MessageBox.Show("Ділення на 0 заборонено!\nБудь-ласка, перевірте значення!");
+                            return;
+                        }
+                        a = Calc.div(x, y);
+                        break;
+                    case (4):
+                        a = Calc.remOfdiv(x, y);
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введене значення не є числом!\nБудь-ласка, перевірте введені значення!");
+                return;
+            }
+
+            label1.Content = a.ToString(format: "");
         }
     }
 }
